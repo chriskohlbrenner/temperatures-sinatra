f21cdec6c713a38a10b4c8f04c1dc38bd8576169
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-03-31 15:13:16 -0400

    fix

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,18 +4,14 @@ class ForecastsController < ApplicationController
   end
 
   get '/forecasts' do
-    @forecast = Forecast.all.last
+    forecast_zipcode = "60187"
+    @forecast = Forecast.new(:zipcode => forecast_zipcode)
+    @scraper = Scraper.new(forecast_zipcode)
+    @forecast.location = @scraper.location
+    @forecast.temperature = @scraper.temperature
+    @forecast.save
+
     erb :"forecasts/index.html"
-
-    # weather_zipcode = params[:zipcode] || "10009"
-
-    # @forecast = Forecast.new(:zipcode => weather_zipcode)
-    # @scraper = Scraper.new(weather_zipcode)
-    # @forecast.location = @scraper.location
-    # @forecast.temperature = @scraper.temperature
-    # @forecast.save
-
-    # erb :"forecasts/index.html"
   end
 
   post '/forecasts' do
@@ -25,25 +21,10 @@ class ForecastsController < ApplicationController
     @forecast.temperature = @scraper.temperature
 
     if @forecast.save
-      redirect "/forecasts"
+      erb :"forecasts/index.html"
     else
       "Sorry, that's not a valid zipcode.<br><a href="">Back to the homepage.</a>"
     end
   end
 
 end
-
-
-  # get '/forecasts' do
-  #   if request.location.data["zipcode"] != ""
-  #     zipcode = request.location.data["zipcode"]
-  #     @scraper = Scraper.new(zipcode)
-  #     @forecast = Forecast.new(:zipcode => zipcode)
-  #     @forecast.location = @scraper.location
-  #     @forecast.temperature = @scraper.temperature
-  #   else
-  #     @scraper = Scraper.new("10001")
-  #     @forecast = Forecast.new(:zipcode => "10001")
-  #     @forecast.location = @scraper.location
-  #     @forecast.temperature = @scraper.temperature
-  #   end
