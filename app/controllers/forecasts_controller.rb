6e6bf7b3230eb69c233df8e663a1af1de81dd3d0
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-04-02 19:29:42 -0400

    ok

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,8 +5,8 @@ class ForecastsController < ApplicationController
 
   get '/forecasts' do
     # if request.location.data['zipcode'] != ""
-      request = Rack::Request.new(env)
-      
+      @request = Rack::Request.new(env)
+      @location = @request.location
       forecast_zipcode = request.location.data['zipcode']
     # else
       # forecast_zipcode =  "10001"
