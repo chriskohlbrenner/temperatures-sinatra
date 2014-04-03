6ebd5b6fd8bdeb5032a2ea28829ce2a7235d8b40
chriskohlbrenner <c.kohlbrenner@gmail.com>
2014-04-02 20:21:48 -0400

    a

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,17 +4,17 @@ class ForecastsController < ApplicationController
   end
 
   get '/forecasts' do
-    request = Rack::Request.new(env)
-    latlong = request.location.data['latitude'] + "," + request.location.data['longitude']
-    binding.pry
-    if Geocoder.search(latlong)[0]
-      @forecast_zipcode = Geocoder.search(latlong)[0].data["address_components"].last["long_name"]
-    else
-      @forecast_zipcode = "10001"
-    end
+    # request = Rack::Request.new(env)
+    # latlong = request.location.data['latitude'] + "," + request.location.data['longitude']
+    # binding.pry
+    # if Geocoder.search(latlong)[0]
+    #   @forecast_zipcode = Geocoder.search(latlong)[0].data["address_components"].last["long_name"]
+    # else
+    #   @forecast_zipcode = "10001"
+    # end
     # forecast_zipcode =
     # else
-      # forecast_zipcode =  "10001"
+      forecast_zipcode =  "10001"
     # end
 
     @forecast = Forecast.new(:zipcode => forecast_zipcode)
